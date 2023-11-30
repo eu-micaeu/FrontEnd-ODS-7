f13146c7e1cc7ec280e340d2900c4ebb76bff629
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-11-30 14:00:22 -0300

    add: Adicionando objetos na lista.

--- js/form.js
+++ js/form.js
@@ -4,72 +4,90 @@ var usuarios = [];
 var id = 0;
 
 // 1. Função adequada para incluir dados do formulário em uma lista e no local storage.
-function incluirDados(){
+function incluirDados() {
 
     var date = new Date();
 
-    var dia = date.getDate(); //Recebendo o dia do sistema usuário
-
-    var mes = date.getMonth() + 1; //Recebendo o mês do sistema usuário
-
-    var ano = date.getFullYear(); //Recebendo o ano do sistema do usuário
-
     id++;
 
-    var nome = document.getElementById("nome").value; // Armazenando o campo "nome" do formulário
+    var nome = document.getElementById("nome").value;
 
-    var email = document.getElementById("email").value; // Armazenando o campo "email" do formulário
+    var email = document.getElementById("email").value;
 
-    var telefone = document.getElementById("telefone").value; // Armazenando o campo "telefone" do formulário
+    var telefone = document.getElementById("telefone").value;
 
-    var idade = document.getElementById("idade").value; // Armazenando o campo "idade" do formulário
+    var idade = document.getElementById("idade").value;
 
-    var instituicao = document.getElementById("instituicao").value; // Armazenando o campo "instituicao" do formulário
+    var instituicao = document.getElementById("instituicao").value;
 
-    var usuario = [id, nome, email, telefone, idade, instituicao, date]; // Criando um usuario com os campos anteriores
+    var usuario = {
 
-    usuarios.push(usuario); // Adicionando o usuário na lista de usuários
+        id: id,
 
-    localStorage.setItem("usuarios", JSON.stringify(usuarios)); //Armazenando todos os usuários no LocalStorage da página
+        nome: nome,
 
-    lista = document.getElementById("lista"); //Buscando o elemento HTML "lista"
+        email: email,
 
-    var li = document.createElement("li"); //Criação de um novo item da lista
+        telefone: telefone,
 
-    // Convertendo  todos os dados para string e armazenando no novo item 
-    li.textContent = "Data de Envio: " + date.toLocaleDateString() + " | ID: " + id + " | Nome: " + nome + " | E-mail: " + email + " | Telefone: " + telefone + " | Idade: " + idade + " | Instituição: " + instituicao;
+        idade: idade,
 
-    lista.appendChild(li); // Adicionando o novo item na lista
+        instituicao: instituicao,
 
-    limparCampos(); //Limpando os campos do formulário
+        dataEnvio: date.toLocaleDateString()
 
+    };
+
+    usuarios.push(usuario);
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    var lista = document.getElementById("lista");
+
+    var li = document.createElement("li");
+
+    li.textContent = "Data de Envio: " + usuario.dataEnvio + " | ID: " + usuario.id + " | Nome: " + usuario.nome + " | E-mail: " + usuario.email + " | Telefone: " + usuario.telefone + " | Idade: " + usuario.idade + " | Instituição: " + usuario.instituicao;
+
+    lista.appendChild(li);
+
+    limparCampos();
 }
+
 
 // 2. Função adequada para excluir um item da lista e do local storage.
 function excluirUmUsuario() {
-    
-    var id = document.getElementById("excluirUmUsuarioPeloID").value; //Busca o item da lista com aquele identificador digitado
 
-    var usuarioFiltrado = usuarios.find(function(usuario) {
+    var id = document.getElementById("excluirUmUsuarioPeloID").value;
 
-        return usuario[0] == id; // Itera entre os usuários e retorna aquele com o mesmo "id" 
+    var usuarioFiltrado = usuarios.find(function (usuario) {
+
+        return usuario.id == id;
 
     });
 
     var index = usuarios.indexOf(usuarioFiltrado);
 
-    usuarios.splice(index, 1); //Remove o usuário selecionado da lista de usuários
+    if (index !== -1) {
 
-    localStorage.setItem("usuarios", JSON.stringify(usuarios)); //Renova a lista de usuários no LocalStorage
+        usuarios.splice(index, 1);
 
-    recarregarLista(); // Recarrega a lista atualizada com todos os usuários
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-    limparExcluirUmUsuario(); // Zera o campo "excluiUmUsuarioPeloID"
+        recarregarLista();
+
+        limparExcluirUmUsuario();
+
+    } else {
+
+        alert("Usuário não encontrado com o ID fornecido.");
+
+    }
 
 }
 
+
 // 3. Função adequada para excluir todos os itens da lista e do local storage.
-function excluirTodosUsuarios(){
+function excluirTodosUsuarios() {
 
     localStorage.clear(); //Limpa o localStorage completamente
 
@@ -85,27 +103,28 @@ function excluirTodosUsuarios(){
 
 // 4. Função adequada para pesquisar um campo do formulário.
 function pesquisarPorNome() {
-    var nomePesquisado = document.getElementById("nomePesquisado").value; //Recebe o valor do nome digitado
 
-    var lista = document.getElementById("lista"); // Busca o elemento HTML "lista"
+    var nomePesquisado = document.getElementById("nomePesquisado").value;
 
-    lista.innerHTML = ""; //Reseta a lista
+    var lista = document.getElementById("lista");
 
-    //Alimenta a lista apenas com o usuário pesquisado utilizando o nome como meio de busca
-    usuarios.forEach(function(usuario) {
+    lista.innerHTML = "";
 
-        if (usuario[1].toLowerCase().includes(nomePesquisado.toLowerCase())) {
+    usuarios.forEach(function (usuario) {
+
+        if (usuario.nome.toLowerCase().includes(nomePesquisado.toLowerCase())) {
 
             var li = document.createElement("li");
 
-            li.textContent = "ID: " + usuario[0] + " | Nome: " + usuario[1] + " | E-mail: " + usuario[2] + " | Telefone: " + usuario[3] + " | Idade: " + usuario[4] + " | Instituição: " + usuario[5];
-
+            li.textContent = "Data de Envio: " + usuario.dataEnvio + " | ID: " + usuario.id + " | Nome: " + usuario.nome + " | E-mail: " + usuario.email + " | Telefone: " + usuario.telefone + " | Idade: " + usuario.idade + " | Instituição: " + usuario.instituicao;
+            
             lista.appendChild(li);
 
         }
-    });
-}
 
+    });
+
+}
 
 // 5. Função adequada para limpar os campos do formulário.
 function limparCampos() {
@@ -131,17 +150,18 @@ function recarregarLista() {
 
     lista.innerHTML = "";
 
-    usuarios.forEach(function(usuario) {
+    usuarios.forEach(function (usuario) {
 
         var li = document.createElement("li");
 
-        li.textContent = "ID: " + usuario[0] + " | Nome: " + usuario[1] + " | E-mail: " + usuario[2] + " | Telefone: " + usuario[3] + " | Idade: " + usuario[4] + " | Instituição: " + usuario[5];
-
+        li.textContent = "Data de Envio: " + usuario.dataEnvio + " | ID: " + usuario.id + " | Nome: " + usuario.nome + " | E-mail: " + usuario.email + " | Telefone: " + usuario.telefone + " | Idade: " + usuario.idade + " | Instituição: " + usuario.instituicao;
+        
         lista.appendChild(li);
 
     });
 
 }
+
 
 //Zera o valor do identificador selecionado
 function limparExcluirUmUsuario() {
